0287cc4c7a1ab7efdaa38689f0e4d19af0bc6c21
Karthika506 <karthimudduluru3@gmail.com>
2023-12-08 21:00:13 +0530

    initial commit

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,82 @@
+import { useLogin } from "@refinedev/core";
+import { useEffect, useRef } from "react";
+
+import { yariga } from "assets";
+
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import { ThemedTitleV2 } from "@refinedev/mui";
+
+import { CredentialResponse } from "../interfaces/google";
+
+// Todo: Update your Google Client ID here
+const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
+export const Login: React.FC = () => {
+  const { mutate: login } = useLogin<CredentialResponse>();
+
+  const GoogleButton = (): JSX.Element => {
+    const divRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      if (typeof window === "undefined" || !window.google || !divRef.current) {
+        return;
+      }
+
+      try {
+        window.google.accounts.id.initialize({
+          ux_mode: "popup",
+          client_id: GOOGLE_CLIENT_ID,
+          callback: async (res: CredentialResponse) => {
+            if (res.credential) {
+              login(res);
+            }
+          },
+        });
+        window.google.accounts.id.renderButton(divRef.current, {
+          theme: "filled_blue",
+          size: "medium",
+          type: "standard",
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }, []);
+
+    return <div ref={divRef} />;
+  };
+
+  return (
+    <Box
+      component="div"
+      sx={{backgroundColor: "#FCFCFC",}}
+    >
+      <Container
+        component="main"
+        maxWidth="xs"
+        sx = {{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          height: "100vh",
+        }}
+      >
+        <Box
+          display="flex"
+          gap="36px"
+          justifyContent="center"
+          flexDirection="column"
+          alignItems= "center"
+        >
+          <div>
+            <img src={yariga} alt="Yariga Logo" />
+          </div>
+          <Box mt = {4}>
+            <GoogleButton />
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
